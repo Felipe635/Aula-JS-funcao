6cee321b64523d73b150458e94128266741d9e66
Felipe635 <102757641+Felipe635@users.noreply.github.com>
2022-05-05 15:07:42 -0300

    Add files via upload

---
+++ exerfuncao.js
@@ -0,0 +1,38 @@
+console.log('Exercicío 1');
+
+function nome (n){
+    console.log(n);
+    return;
+}
+
+nome('Felipe');
+
+console.log('***************');
+console.log('Exercicío 2');
+
+function soma (a,b){
+    console.log(a+b);
+    return;
+}
+
+soma(5,3);
+
+console.log('***************');
+console.log('Exercicío 3');
+
+function  mult (c,d){
+    console.log(c*d);
+    return;
+}
+
+mult(2,2);
+
+console.log('***************');
+console.log('Exercicío 4');
+
+function media (n1,n2,n3,n4){
+    console.log((n1+n2+n3+n4)/4);
+    return;
+}
+
+media(1,1,1,1);
